2dc9feec631e8321ec02a34ac53504593cb90224
h4r3xy <86131500+h4r3xy@users.noreply.github.com>
2021-06-18 23:36:16 +0200

    Add files via upload

---
+++ harexOS_1.1.py
@@ -0,0 +1,41 @@
+print("╔╗─╔╗───────────╔═══╦═══╗")
+print("║║─║║───────────║╔═╗║╔═╗║")
+print("║╚═╝╠══╦═╦══╦╗╔╗║║─║║╚══╗")
+print("║╔═╗║╔╗║╔╣║═╬╬╬╝║║─║╠══╗║")
+print("║║─║║╔╗║║║║═╬╬╬╗║╚═╝║╚═╝║")
+print("╚╝─╚╩╝╚╩╝╚══╩╝╚╝╚═══╩═══╝")
+
+input("Press ENTER to continue")
+
+
+print("Welcome to Harex OS")
+name = input("Enter your name: ")
+print("Hello " + name + "!")
+
+age = input("Enter your age: ")
+print(age + "? " + "cool age :)")
+
+user = input("Your Username: ")
+print(user + " Username Set")
+
+print("ENTERING DESKTOP...")
+print("╔═══╦═══╦═══╦╗╔═╦════╦═══╦═══╗")
+print("╚╗╔╗║╔══╣╔═╗║║║╔╣╔╗╔╗║╔═╗║╔═╗║")
+print("─║║║║╚══╣╚══╣╚╝╝╚╝║║╚╣║─║║╚═╝║")
+print("─║║║║╔══╩══╗║╔╗║──║║─║║─║║╔══╝")
+print("╔╝╚╝║╚══╣╚═╝║║║╚╗─║║─║╚═╝║║")
+print("╚═══╩═══╩═══╩╝╚═╝─╚╝─╚═══╩╝")
+print("")
+print("Entering Desktop Succesfully ")
+
+
+
+
+
+
+
+
+
+
+
+
